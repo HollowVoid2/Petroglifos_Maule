e6b01d00ea200f68fbb79caf0f4b663ea2116023
Hollow_void <victorhgemgigr@gmail.com>
2026-07-02 03:42:56 -0400

    Arreglos menores pero importantes

--- gestion/urls.py
+++ gestion/urls.py
@@ -11,9 +11,6 @@ urlpatterns = [
     # === RUTAS PRIVADAS (requieren login) ===
     path('mis-investigaciones/', views.catalogo_mis_investigaciones, name='mis_investigaciones'),
     path('catalogo/', views.catalogo_todas_investigaciones, name='catalogo_todas'),
-    #el resto igual
-    path('', views.catalogo_mis_investigaciones, name='mis_investigaciones'),
-    path('catalogo/', views.catalogo_todas_investigaciones, name='catalogo_todas'),
     path('simbolo/<int:simbolo_id>/', views.simbolo_detalle, name='simbolo_detalle'),
     path('simbolo/nuevo/', views.crear_simbolo, name='crear_simbolo'),
     path('simbolo/<int:simbolo_id>/editar/', views.editar_simbolo, name='editar_simbolo'),
